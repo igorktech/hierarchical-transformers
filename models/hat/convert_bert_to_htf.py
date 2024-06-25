10e20e765d786e90b78fd38d69341f8dc511dd47
Igor <68161226+igorktech@users.noreply.github.com>
2024-06-25 12:47:09 +0200

    Update convert_bert_to_htf.py

--- models/hat/convert_bert_to_htf.py
+++ models/hat/convert_bert_to_htf.py
@@ -106,22 +106,37 @@ def convert_bert_to_htf():
 
     if config.warmup_strategy != 'none':
         # copy embeddings
-        htf_model.hi_transformer.embeddings.position_embeddings.weight.data[0] = torch.zeros((bert_config.hidden_size,))
-        htf_model.hi_transformer.embeddings.position_embeddings.weight.data[1:] = bert_model.bert.embeddings.position_embeddings.weight[1:MAX_SENTENCE_LENGTH+htf_config.pad_token_id+1]
-        htf_model.hi_transformer.embeddings.word_embeddings.load_state_dict(bert_model.bert.embeddings.word_embeddings.state_dict())
-        htf_model.hi_transformer.embeddings.token_type_embeddings.load_state_dict(bert_model.bert.embeddings.token_type_embeddings.state_dict())
-        htf_model.hi_transformer.embeddings.LayerNorm.load_state_dict(bert_model.bert.embeddings.LayerNorm.state_dict())
+        htf_model.hi_transformer.embeddings.position_embeddings.weight.data[0] = torch.zeros((htf_config.hidden_size,))
+        if bert_config.model_type == 'bert':
+            htf_model.hi_transformer.embeddings.position_embeddings.weight.data[1:] = bert_model.bert.embeddings.position_embeddings.weight[1:MAX_SENTENCE_LENGTH+htf_config.pad_token_id+1]
+            htf_model.hi_transformer.embeddings.word_embeddings.load_state_dict(bert_model.bert.embeddings.word_embeddings.state_dict())
+            htf_model.hi_transformer.embeddings.token_type_embeddings.load_state_dict(bert_model.bert.embeddings.token_type_embeddings.state_dict())
+            htf_model.hi_transformer.embeddings.LayerNorm.load_state_dict(bert_model.bert.embeddings.LayerNorm.state_dict())
+        if bert_config.model_type == 'distilbert':
+            htf_model.hi_transformer.embeddings.position_embeddings.weight.data[1:] = bert_model.distilbert.embeddings.position_embeddings.weight[1:MAX_SENTENCE_LENGTH+htf_config.pad_token_id+1]
+            htf_model.hi_transformer.embeddings.word_embeddings.load_state_dict(bert_model.distilbert.embeddings.word_embeddings.state_dict())
+            # htf_model.hi_transformer.embeddings.token_type_embeddings.load_state_dict(bert_model.distilbert.embeddings.token_type_embeddings.state_dict())
+            htf_model.hi_transformer.embeddings.LayerNorm.load_state_dict(bert_model.distilbert.embeddings.LayerNorm.state_dict())
 
         if config.warmup_strategy != 'embeds-only':
             # copy transformer layers
             if config.warmup_strategy != 'linear':
                 for idx in range(NUM_HIDDEN_LAYERS):
                     if htf_model.config.encoder_layout[str(idx)]['sentence_encoder']:
-                        htf_model.hi_transformer.encoder.layer[idx].sentence_encoder.load_state_dict(bert_model.bert.encoder.layer[idx].state_dict())
+                        if bert_config.model_type == 'bert':
+                            htf_model.hi_transformer.encoder.layer[idx].sentence_encoder.load_state_dict(bert_model.bert.encoder.layer[idx].state_dict())
+                        if bert_config.model_type == 'distilbert':
+                            htf_model.hi_transformer.encoder.layer[idx].sentence_encoder.load_state_dict(bert_model.distilbert.transformer.layer[idx].state_dict())
                     if htf_model.config.encoder_layout[str(idx)]['document_encoder']:
                         if config.warmup_strategy == 'grouped':
-                            htf_model.hi_transformer.encoder.layer[idx].document_encoder.load_state_dict(bert_model.bert.encoder.layer[idx].state_dict())
-                        htf_model.hi_transformer.encoder.layer[idx].position_embeddings.weight.data = bert_model.bert.embeddings.position_embeddings.weight[1:MAX_SENTENCES+2]
+                            if bert_config.model_type == 'bert':
+                                htf_model.hi_transformer.encoder.layer[idx].document_encoder.load_state_dict(bert_model.bert.encoder.layer[idx].state_dict())
+                            if bert_config.model_type == 'distilbert':
+                                htf_model.hi_transformer.encoder.layer[idx].document_encoder.load_state_dict(bert_model.distilbert.transformer.layer[idx].state_dict())
+                        if bert_config.model_type == 'bert':
+                                htf_model.hi_transformer.encoder.layer[idx].position_embeddings.weight.data = bert_model.bert.embeddings.position_embeddings.weight[1:MAX_SENTENCES+2]
+                        if bert_config.model_type == 'distilbert':
+                            htf_model.hi_transformer.encoder.layer[idx].position_embeddings.weight.data = bert_model.distilbert.embeddings.position_embeddings.weight[1:MAX_SENTENCES+2]
             else:
                 for idx, l_idx in enumerate(range(0, NUM_HIDDEN_LAYERS*2, 2)):
                     if htf_model.config.encoder_layout[str(idx)]['sentence_encoder']:
